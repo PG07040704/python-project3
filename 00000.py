c95bd8e4834185d16ea08a9c0740db8dd983c9ed
PG07040704 <164132846+PG07040704@users.noreply.github.com>
2025-06-08 22:05:56 +0800

    Add files via upload

---
+++ 00000.py
@@ -0,0 +1,8 @@
+import keras
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+
+print("Keras 版本:", keras.__version__)
+print("TensorFlow 版本:", tf.__version__)
+print("NumPy 版本:", np.__version__)
